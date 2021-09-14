25ba232b7aead3e2b38fc967754a3040bde09a78
Cwekii <dominik.cvetkovski@gmail.com>
2021-09-14 13:08:07 +0200

    Transform 4

---
+++ Programiranje/01_Transform/3_Zadatci/Zadatak_3_2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Napišite skriptu koja kocku povećava ili smanjuje za javne vrijednosti u beskonaćnost
+
+public class Zadatak_3_2 : MonoBehaviour
+{
+    public float xScale;
+    public float yScale;
+    public float zScale;
+
+    public bool increase = true;
+    public bool decsrease = false;
+
+    private void Update()
+    {
+        if(increase == true && decsrease == false)
+        {
+            transform.localScale += new Vector3(xScale, yScale, zScale);
+        }
+        else if(decsrease == true && increase == false)
+        {
+            transform.localScale -= new Vector3(xScale, yScale, zScale);
+        }
+        else
+        {
+            Debug.Log("Nisi odabrao hoćeš li da se objekt smanjuje ili povećava");
+        }
+    }
+}
