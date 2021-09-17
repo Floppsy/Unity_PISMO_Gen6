d5120686337872a1a9e71e4a150acf26a5d0b225
Cwekii <dominik.cvetkovski@gmail.com>
2021-09-17 09:27:23 +0200

    Colliders

---
+++ Programiranje/03_Colliders/Collisions/Collision_Metode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision_Metode : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Dotakli su se");
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Diraju se");
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Više se ne diraju :(");
+    }
+}
