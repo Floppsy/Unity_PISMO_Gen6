d3f0f7b0d17f0444b0d588150a4e1ac2964808ca
Cwekii <dominik.cvetkovski@gmail.com>
2021-09-20 12:27:01 +0200

    Zadatci 2 game object

---
+++ Programiranje/05_GameObject/1_Zadatci/Zadatak_7.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Napravite objekt koji se krece iskljucivo lijevo ili desno (Z os)
+ * na tipke strelice left arrow i right arrow.
+ * Kada dotakne sferu neka se ugasi sfera i aktiviraju sve kapsule,
+ * a kada dotakne jednu kapsulu neka ugasi sve kapsule koje jesu aktivne.
+ * Cilj je ugasiti sve objekte na sceni.
+ * Imajte 4 kapsule i 2 sfere (sve su naravno na jednakoj Y i X osi)
+ */
+
+public class Zadatak_7 : MonoBehaviour
+{
+    public GameObject sphere1;
+    public GameObject sphere2;
+
+    public GameObject kaps1;
+    public GameObject kaps2;
+    public GameObject kaps3;
+    public GameObject kaps4;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        kaps1.SetActive(false);
+        kaps2.SetActive(false);
+        kaps3.SetActive(false);
+        kaps4.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(Vector3.right);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(Vector3.left);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Sphere")
+        {
+            other.gameObject.SetActive(false);
+            kaps1.SetActive(true);
+            kaps2.SetActive(true);
+            kaps3.SetActive(true);
+            kaps4.SetActive(true);
+        }
+        else if(other.gameObject.name == "Capsule")
+        {
+            kaps1.SetActive(false);
+            kaps2.SetActive(false);
+            kaps3.SetActive(false);
+            kaps4.SetActive(false);
+        }
+    }
+}
---
+++ Programiranje/05_GameObject/1_Zadatci/Zadatak_8.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Napravite skriptu koja svaki frame pali/gasi objekt.
+ * Na tipku F skripta to prestaje raditi.
+ * Kada se stisne opet tipka F skripta opet o počinje raditi (obavezno koristiti vlastitu metodu)
+ */
+
+public class Zadatak_8 : MonoBehaviour
+{
+    bool radi = true;
+
+    public GameObject objekt;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            radi = !radi; //promjeiti ce se vrijednost radi ako je bio true biti ce false a ako je bio false onda true
+        }
+        if(radi)
+        {
+            PaliGasi();
+        }
+    }
+
+    void PaliGasi()
+    {
+        if (objekt.activeSelf == true)
+        {
+            objekt.SetActive(false);
+        }
+        else if (objekt.activeSelf == false)
+        {
+            objekt.SetActive(true);
+        }
+    }
+}
