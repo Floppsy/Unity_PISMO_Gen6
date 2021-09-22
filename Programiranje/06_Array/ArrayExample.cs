10e163a2ad213738c399b1b9b82505f71df2e981
Cwekii <dominik.cvetkovski@gmail.com>
2021-09-22 09:37:54 +0200

    Array

---
+++ Programiranje/06_Array/ArrayExample.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayExample : MonoBehaviour
+{
+    public GameObject[] prefabs;
+    public int brojStvaranja;
+
+    private void Start()
+    {
+        Instantiate(prefabs[brojStvaranja], new Vector3(0, 5, 0), Quaternion.identity);
+    }
+}
---
+++ Programiranje/06_Array/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject target; //To smo mi kao player a ovdje cemo oznaciti da ide prema nama (game objectu)
+    public float speed;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        transform.LookAt(target.transform);
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+    }
+}
