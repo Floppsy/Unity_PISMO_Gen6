476380c6feeeb0d6579dd2369060bada5a169cc3
Cwekii <dominik.cvetkovski@gmail.com>
2021-09-29 09:47:16 +0200

    Collecting objects game & level design update & timer script

---
+++ Games/02_CollectObjects/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int pickedArtifects;
+    public GameObject[] artifacts;
+    public UIManager uiM;
+
+    private void Start()
+    {
+        uiM = FindObjectOfType<UIManager>();
+        artifacts = GameObject.FindGameObjectsWithTag("Artifact");
+        //Želim da mi ispiše koliko od koliko artifekata imam - to treba napraviti UI Manager
+        uiM.UpdateText(pickedArtifects, artifacts.Length);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Artifact")
+        {
+            pickedArtifects++;
+            uiM.UpdateText(pickedArtifects, artifacts.Length);
+            if(pickedArtifects == artifacts.Length)
+            {
+                uiM.ShowEndGame(pickedArtifects);
+                Time.timeScale = 0;
+                //GameObject player = GameObject.FindGameObjectWithTag("Player");
+                //player.SetActive(false);
+            }
+            other.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Programiranje/08_Timer/Timer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText; //Prikaz teksta
+    [Range(0, 59)]
+    public int timeCountMinutes = 5; //Unešena vrijednost u minuta od koliko krecemo npr. imamo 5 minuta za skupiti sve sfere
+    [Range(0, 59)]
+    public int timeCountSeconds;
+    public float allTime;
+    public Collect ct;
+
+    private void Start()
+    {
+        ct = FindObjectOfType<Collect>();
+        timeCountMinutes *= 60; //Pretvorili smo minute u sekunda
+        allTime = timeCountMinutes + timeCountSeconds;
+    }
+
+    private void Update()
+    {
+        if(allTime >= 0)
+        {
+            allTime -= Time.deltaTime;
+
+            //Djelimo sa cijelim brojem 60 da dobijemo minute
+            int minutes = (int)(allTime / 60);
+            //Djelimo sa 60 da dobijemo ostatak kako bismo dobili sekunde
+            //int seconds = (int)(timeCount % 60);
+            int seconds = Mathf.FloorToInt(allTime % 60);
+            
+            //Ako je manje od 10 za minute i sekunde
+            if(minutes < 10 && seconds < 10)
+            {
+                timerText.text = "0" + minutes + ":" + "0" + seconds;
+            }
+            //Ako su minute jednoznamenkaste, a sekunde dvoznamenkaste
+            else if(minutes < 10 && seconds >= 10)
+            {
+                timerText.text = "0" + minutes + ":" + seconds;
+            }
+            //Minute su dvoznamenkaste, ali sekunde su jednoznamenkaste
+            else if(minutes >= 10 && seconds < 10)
+            {
+                timerText.text = minutes + ":0" + seconds;
+            }
+            //Minute i sekunde su nam dvoznamenkaste
+            else
+            {
+                timerText.text = minutes + ":" + seconds;
+            }
+        }
+        else
+        {
+            ct.Lose();
+        }
+    }
+}
