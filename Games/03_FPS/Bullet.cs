3c9e74117d8569636957d6a7ec5b8de577c50fc2
Cwekii <dominik.cvetkovski@gmail.com>
2021-10-01 11:43:03 +0200

    Gun i Bullet FPS

---
+++ Games/03_FPS/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour
+{
+    Rigidbody rb;
+    public float damage;
+    public float speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            //Skinuti health enemyu
+            Destroy(this.gameObject);
+        }
+        else if(collision.gameObject.tag != "Enemy")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Games/03_FPS/Gun.cs
+++ Games/03_FPS/Gun.cs
@@ -14,7 +14,7 @@ public class Gun : MonoBehaviour
     [Header("About Weapon:")]
     public float fireRate;
     float fireRateReset;
-    public float accuracy;
+    public float accuracy = 1;
     public float reloadTime;
     float reloadTimeReset;
     public float recoil;
@@ -25,15 +25,67 @@ public class Gun : MonoBehaviour
     public Rigidbody bulletPrefab;
     public Transform bulletSpawnPosition;
     AudioSource bulletSoundSource;
-    //Bullet script
+    Bullet bulletScript;
 
-    [Header("Fire mode:")]
-    public bool singleFire = true;
-    public bool automaticFire;
-    public bool burstFire;
+    [Header("Fire mode: *Signle Fire is defoult*")]
+    public bool singleFire = true; //0
+    public bool automaticFire; //1
+    public bool burstFire; //2
+    int fireMode = 0;
 
     private void Start()
     {
-        
+        currentAmmo = maxAmmo;
+        ShowAmmo();
+        fireRateReset = fireRate;
+        reloadTimeReset = reloadTime;
+        bulletSoundSource = GetComponent<AudioSource>();
+        bulletScript = bulletPrefab.gameObject.GetComponent<Bullet>();
+
+        if(singleFire)
+        {
+            fireMode = 0;
+        }
+        else if(automaticFire)
+        {
+            fireMode = 1;
+        }
+        else if(burstFire)
+        {
+            fireMode = 2;
+        }
+        else
+        {
+            fireMode = 0;
+        }
+    }
+
+    private void Update()
+    {
+        fireRate -= Time.deltaTime;
+        reloadTime -= Time.deltaTime;
+
+        if(Input.GetMouseButtonDown(0) && fireMode == 0 && currentAmmo > 0 && fireRate <= 0)
+        {
+            float xScreen = Screen.width / 2;
+            float yScreen = Screen.height / 2;
+
+            float xAcc = Random.Range(xScreen - accuracy, xScreen + accuracy);
+            float yAcc = Random.Range(yScreen - accuracy, yScreen + accuracy);
+
+            var ray = Camera.main.ScreenPointToRay(new Vector3(xAcc, yAcc, 0));
+
+            currentAmmo--;
+            ShowAmmo();
+
+            Rigidbody cloneBullet;
+            cloneBullet = Instantiate(bulletPrefab, bulletSpawnPosition.position, bulletSpawnPosition.rotation);
+            cloneBullet.velocity = bulletScript.speed * ray.direction;
+        }
+    }
+
+    public void ShowAmmo()
+    {
+        ammoText.text = currentAmmo + "/" + maxAmmo;
     }
 }
