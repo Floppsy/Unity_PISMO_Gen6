8dabd640fb53efb2339451b590f743684ccd25d8
Cwekii <dominik.cvetkovski@gmail.com>
2021-10-01 10:37:57 +0200

    FPS

---
+++ Games/03_FPS/Gun.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class Gun : MonoBehaviour
+{
+    [Header("Ammo:")]
+    public int maxAmmo;
+    int currentAmmo;
+    public Text ammoText;
+
+    [Header("About Weapon:")]
+    public float fireRate;
+    float fireRateReset;
+    public float accuracy;
+    public float reloadTime;
+    float reloadTimeReset;
+    public float recoil;
+    public Camera mainCamera;
+    public Camera scopeCamera;
+
+    [Header("Bullet info:")]
+    public Rigidbody bulletPrefab;
+    public Transform bulletSpawnPosition;
+    AudioSource bulletSoundSource;
+    //Bullet script
+
+    [Header("Fire mode:")]
+    public bool singleFire = true;
+    public bool automaticFire;
+    public bool burstFire;
+
+    private void Start()
+    {
+        
+    }
+}
