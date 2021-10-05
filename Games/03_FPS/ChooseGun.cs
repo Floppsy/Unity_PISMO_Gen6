d814f33bc6cb8baabdc1dd020dde6ba119413bcb
Cwekii <dominik.cvetkovski@gmail.com>
2021-10-05 10:42:56 +0200

    FPS Done with package

---
+++ Games/03_FPS/ChooseGun.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseGun : MonoBehaviour
+{
+    public GameObject[] guns;
+
+    private void Start()
+    {
+        for (int i = 0; i < guns.Length; i++)
+        {
+            guns[i].SetActive(false);
+        }
+        guns[0].SetActive(true);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            for(int i = 0; i < guns.Length; i++)
+            {
+                guns[i].SetActive(false);
+            }
+            guns[0].SetActive(true);
+            var gun = guns[0].GetComponent<Gun>();
+            gun.ShowAmmo();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            for (int i = 0; i < guns.Length; i++)
+            {
+                guns[i].SetActive(false);
+            }
+            guns[1].SetActive(true);
+            var gun = guns[1].GetComponent<Gun>();
+            gun.ShowAmmo();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            for (int i = 0; i < guns.Length; i++)
+            {
+                guns[i].SetActive(false);
+            }
+            guns[2].SetActive(true);
+            var gun = guns[2].GetComponent<Gun>();
+            gun.ShowAmmo();
+        }
+    }
+}
---
+++ Games/03_FPS/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public Transform[] spawnPoints;
+    public float timer = 3.14f;
+    float timerReset;
+
+    private void Start()
+    {
+        timerReset = timer;
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            Instantiate(enemies[Random.Range(0, enemies.Length)], spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+            timer = timerReset;
+        }
+    }
+}
