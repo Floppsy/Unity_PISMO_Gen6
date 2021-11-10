0e64a87e0485bef337345b811c1fec84fc9d9974
Cwekii <dominik.cvetkovski@gmail.com>
2021-11-10 13:52:13 +0100

    AR

---
+++ Programiranje/14_AR/AddNewTarget.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddNewTarget : MonoBehaviour
+{
+    public Text countText;
+    public int countObjects = 0;
+    public bool auto = false;
+    public bool marko = false;
+
+    public void FoundCar()
+    {
+        if(auto == false)
+        {
+            auto = true;
+            countObjects++;
+            countText.text = countObjects + "/10";
+        }
+    }
+
+    public void FoundMarko()
+    {
+        if (marko == false)
+        {
+            marko = true;
+            countObjects++;
+            countText.text = countObjects + "/10";
+        }
+    }
+}
